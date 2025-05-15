a59620b3f5acb437f5f7caa981de44b0c95b2b80
Friars23707 <147444129+Friars23707@users.noreply.github.com>
2025-05-15 15:12:29 -0400

    Create hi.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hi.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class hi extends LinearOpMode {
+
+    DcMotor leftFront;
+    DcMotor leftBack;
+    DcMotor rightFront;
+    DcMotor rightBack;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightBack = hardwareMap.get(DcMotor.class, "leftFront");
+
+
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double lateral = gamepad1.left_stick_x;
+            double yaw = gamepad1.right_stick_x;
+
+
+            double leftFrontPower = axial + lateral + yaw;
+            double rightFrontPower = axial - lateral - yaw;
+            double leftBackPower = axial - lateral + yaw;
+            double rightBackPower = axial + lateral - yaw;
+
+
+            leftFront.setPower(leftFrontPower);
+            leftBack.setPower(rightFrontPower);
+            rightFront.setPower(leftBackPower);
+            rightBack.setPower(rightBackPower);
+        }
+
+    }
+}
